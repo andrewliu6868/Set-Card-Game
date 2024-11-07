2b860a929358b2963a8462122baf567edc42ae41
andrewliu6868 <andrewliu6867@gmail.com>
2024-11-06 23:31:22 -0800

    Learning about getters, setters and extension

--- Card Game/CardGameModel.swift
+++ Card Game/CardGameModel.swift
@@ -21,19 +21,29 @@ struct MemoryGame<CardContent> where CardContent: Equatable{
         }
     }
     
-    mutating func choose(_ card:Card){
-        let chosenIndex = index(of: card)
-        cards[chosenIndex].isFaceUp.toggle()
+    var indexOfOnlyUpCard: Int? {
+        // getters are for retrieving content within a class
+        get{cards.indices.filter {index in cards[index].isFaceUp}.only}
+        // setters are for altering content within a class, newValue is a builtin parameter
+        set{cards.indices.forEach {cards[$0].isFaceUp = (newValue == $0)}}
     }
     
-    func index (of card:Card) -> Int{
-        for index in cards.indices {
-            if cards[index].id == card.id{
-                return index
+    mutating func choose(_ card:Card){
+        if let chosenIndex = cards.firstIndex(where: {$0.id == card.id}){
+            if !cards[chosenIndex].isFaceUp && !cards[chosenIndex].isMatched{
+                if let currIndex = indexOfOnlyUpCard{
+                    if cards[chosenIndex].content == cards[currIndex].content{
+                        cards[chosenIndex].isMatched = true
+                        cards[currIndex].isMatched = true
+                    }
+                }else{
+                    indexOfOnlyUpCard = chosenIndex
+                }
+                cards[chosenIndex].isFaceUp = true
             }
         }
-        return 0 // FIXME: bogus!
     }
+    
     
     // you must mark mutating when mutating with structs
     mutating func shuffle(){
@@ -42,7 +52,7 @@ struct MemoryGame<CardContent> where CardContent: Equatable{
     }
     
     struct Card : Equatable, Identifiable, CustomDebugStringConvertible {
-        var isFaceUp: Bool = true
+        var isFaceUp: Bool = false
         var isMatched: Bool = false
         // card content shouldn't be changing so use a let
         let content: CardContent
@@ -52,5 +62,12 @@ struct MemoryGame<CardContent> where CardContent: Equatable{
         var debugDescription: String {
             "\(id): \(content) \(isFaceUp ? "up" : "down")\(isMatched ? "matched" : "")"
         }
+    }
+}
+
+// extending array
+extension Array {
+    var only: Element? {
+        count == 1 ? first : nil // don't need to explicitly state array.first because it's assumed you're applying these to arrays
     }
 }
